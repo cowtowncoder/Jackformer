a7d1b0368b2e054aae49d0fa4190a3818fbd3f7d
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-02-05 14:24:01 -0800

    First, Spring initializr (https://start.spring.io) generated version

---
+++ src/main/java/com/cowtowncoder/jackformer/webapp/JackformerApp.java
@@ -0,0 +1,12 @@
+package com.cowtowncoder.jackformer.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackformerApp
+{
+	public static void main(String[] args) {
+		SpringApplication.run(JackformerApp.class, args);
+	}
+}
---
+++ src/test/java/com/cowtowncoder/jackformer/jackformerwebapp/JackformerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowtowncoder.jackformer.jackformerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackformerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
